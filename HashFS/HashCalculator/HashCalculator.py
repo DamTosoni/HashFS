5f006b50fadbb9c99eb194dc7bc5c12a68f78896
Danilo Salvati <sadan91@gmail.com>
2015-01-05 04:33:24 +0100

    Migliorata la struttura per il calcolo dell'hash

--- HashFS/HashCalculator/HashCalculator.py
+++ HashFS/HashCalculator/HashCalculator.py
@@ -10,7 +10,16 @@ class HashCalculator(object):
     Calcolo l'hash di un singolo file
     """
     def calculateFileHash(self, file_path, blocksize=65536):
-        raise NotImplementedError("Metodo non implementato")
+        open_file = open(file_path, 'rb')
+        buf = open_file.read(blocksize)
+        while len(buf) > 0:
+            self._hash_calculator.update(buf)
+            buf = open_file.read(blocksize)
+
+        # Aggiungo il nome del file all'hash
+        self._hash_calculator.update(file_path.encode(encoding='UTF-8'))
+
+        return self._hash_calculator.hexdigest()
 
     """
     Calcolo l'hash di una directory
--- HashFS/HashCalculator/HashCalculatorMD5.py
+++ HashFS/HashCalculator/HashCalculatorMD5.py
@@ -5,21 +5,6 @@ del filesystem usando l'algoritmo MD5
 from HashCalculator import HashCalculator
 class HashCalculatorMD5(HashCalculator):
 
-    def __init__(self, fs_file_path=""):
+    def __init__(self):
         import hashlib
-        self. _hash_calculator = hashlib.md5()
-
-    """
-    Calcolo l'hash di un singolo file
-    """
-    def calculateFileHash(self, file_path, blocksize=65536):
-        open_file = open(file_path, 'rb')
-        buf = open_file.read(blocksize)
-        while len(buf) > 0:
-            self._hash_calculator.update(buf)
-            buf = open_file.read(blocksize)
-
-        # Aggiungo il nome del file all'hash
-        self._hash_calculator.update(file_path.encode(encoding='UTF-8'))
-
-        return self._hash_calculator.hexdigest()
+        self._hash_calculator = hashlib.md5()
